d2088b86dcdb48e2b429c68007a419e011cc7ef4
George <gapetrov_1@edu.hse.ru>
2022-07-06 14:30:33 +0300

    added readme file and time picker

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# securities_dealing
+# Securities dealing
+
+The program displays stock quotes in real time. To run this program you need to download
+file and install all used libraries.
+After that, you will need to run main.py, it will ask you limit of displaying seconds
+and ticker of the companies you want to see. You will see the dynamic graph 
+with tick-by-tick price, like on the screen below. <img src="describe.jpg">
+In the future I will add it on the site with visual design and add 
+machine learning algorithms to analyse price movement
--- broker_connector/stockhistory.py
+++ broker_connector/stockhistory.py
@@ -2,6 +2,9 @@ import numpy as np
 from datetime import datetime, timedelta
 
 
+sec_to_show = int(input("Seconds to display on the graph: "))
+
+
 class StockHistory:
     all_stocks = dict()
 
@@ -20,6 +23,6 @@ class StockHistory:
         self.time_value_pair = np.concatenate((self.time_value_pair, timestamp_price), 0)
         i = 0
         while (datetime.strptime(self.time_value_pair[-1, 0].split('.')[0], '%Y-%m-%d %H:%M:%S')
-               - datetime.strptime(self.time_value_pair[i, 0].split('.')[0], '%Y-%m-%d %H:%M:%S')) >= timedelta(seconds=10):
+               - datetime.strptime(self.time_value_pair[i, 0].split('.')[0], '%Y-%m-%d %H:%M:%S')) >= timedelta(seconds=sec_to_show):
             i += 1
         self.time_value_pair = self.time_value_pair[i:, :]
