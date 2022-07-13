9a49b4bc1c2aed83f29f7197e2c460001d04d7ab
George <gapetrov_1@edu.hse.ru>
2022-07-13 12:43:46 +0300

    not only shares but all currencies allowed

--- broker_connector/ticker_to_figi.py
+++ broker_connector/ticker_to_figi.py
@@ -5,20 +5,22 @@ from tinkoff.invest.services import InstrumentsService
 from .token_file import TOKEN
 
 
-rts = input("Companies ticker to show:").split(",")
+sec_name = input("Tickers to show:").split(",")
 
 
 with Client(TOKEN) as cl:
     instruments: InstrumentsService = cl.instruments
     ticker_to_figi_list = []
 
-    for item in getattr(instruments, 'shares')().instruments:
-        if item.ticker in rts:
-            ticker_to_figi_list.append({
-                'ticker': item.ticker,
-                'figi': item.figi,
-                'name': item.name
-            })
+    for asset_type in ['shares', 'currencies', 'bonds', 'futures']:
+        for item in getattr(instruments, asset_type)().instruments:
+            if item.ticker in sec_name:
+                ticker_to_figi_list.append({
+                    'ticker': item.ticker,
+                    'figi': item.figi,
+                    'name': item.name
+                })
+
 
 df = pd.DataFrame(ticker_to_figi_list)
 print(df)
